90e6c8da0da25f40afc784ec4bb3797f5151740d
etrepauline <92852443+etrepauline@users.noreply.github.com>
2021-10-20 11:23:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+
+$(document).ready(function () {
+
+    // --------------------------------------
+    // --------------------------------------
+    //               TOOLBOXES
+    // --------------------------------------
+    // --------------------------------------
+
+
+    // tooltip de gauche
+    $(".arrow-left").on("mouseenter", function () {
+        $(".toolbox-g").removeClass('hidden');
+    }).on("mouseleave", function () {
+        $(".toolbox-g").addClass('hidden');
+    })
+
+    // tooltip de droite
+    $(".arrow-right").on("mouseenter", function () {
+        $(".toolbox-d").removeClass('hidden');
+    }).on("mouseleave", function () {
+        $(".toolbox-d").addClass('hidden');
+    })
+
+    // --------------------------------------
+    // --------------------------------------
+    //      Hover images (index.html)
+    // --------------------------------------
+    // --------------------------------------
+
+
+})
+
+
+
+
+
+
